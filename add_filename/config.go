51a92dfeb2e6856d263fc6cb7148a299539cf754
mritd <mritd@linux.com>
2020-07-15 16:59:11 +0800

    feat(add_filename): remove logtype
    remove logtype
    
    Signed-off-by: mritd <mritd@linux.com>

--- add_filename/config.go
+++ add_filename/config.go
@@ -1,19 +1,14 @@
 package add_filename
 
-import "regexp"
-
 // config for add_filename processor.
 type config struct {
-	EnableTimestamp bool              `config:"enable_timestamp"`
-	TimestampFormat string            `config:"timestamp_format"`
-	ProcessorsField string            `config:"processors_field"`
-	SourceField     string            `config:"source_field"`
-	TargetField     string            `config:"target_field"`
-	IgnoreMissing   bool              `config:"ignore_missing"`
-	IgnoreFailure   bool              `config:"ignore_failure"`
-	LogTypeField    string            `config:"log_type_field"`
-	LogType         map[string]string `config:"log_type"`
-	logTypeRex      map[string]*regexp.Regexp
+	EnableTimestamp bool   `config:"enable_timestamp"`
+	TimestampFormat string `config:"timestamp_format"`
+	ProcessorsField string `config:"processors_field"`
+	SourceField     string `config:"source_field"`
+	TargetField     string `config:"target_field"`
+	IgnoreMissing   bool   `config:"ignore_missing"`
+	IgnoreFailure   bool   `config:"ignore_failure"`
 }
 
 func defaultConfig() config {

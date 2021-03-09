330041c6cf3fe155ed5c578660a3e3ff58101b34
mritd <mritd@linux.com>
2021-03-09 22:57:32 +0800

    chore(ci): update ci config
    update ci config
    
    Signed-off-by: mritd <mritd@linux.com>

--- .compile.sh
+++ .compile.sh
@@ -2,25 +2,14 @@
 
 set -ex
 
-# create source dir
-mkdir -p ${HOME}/go/src/github.com/elastic
-cd ${HOME}/go/src/github.com/elastic
-
-# get source code
-apt update \
-  && apt upgrade -y \
-  && apt install git -y \
-  && git clone https://github.com/elastic/beats.git
-
 # checkout
-cd ${HOME}/go/src/github.com/elastic/beats
-git checkout -b ${VERSION} ${VERSION}
+(cd beats && git checkout -b ${FILEBEAT_VERSION} ${FILEBEAT_VERSION})
 
 # add custom processors
-cat ${HOME}/filebeat-processors/import.txt >> libbeat/cmd/instance/imports_common.go
+cat filebeat-processors/import.txt >> beats/libbeat/cmd/instance/imports_common.go
 
 # build
-cd filebeat && make crosscompile && mv build/bin/* ${HOME}/build
+(cd beats/filebeat && make crosscompile && mv build/bin/* ../../build)
 
 # test processors
-${HOME}/build/filebeat-linux-amd64 test config -c ${HOME}/filebeat-processors/filebeat-test.yml
+./build/filebeat-linux-amd64 test config -c filebeat-processors/filebeat-test.yml
